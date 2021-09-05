d23f1969abe0f958098f7daa0aa63277bf1a4d1b
Ahmed Foysal <ahmedfoysal01797@gmail.com>
2021-09-05 06:29:50 +0600

    updated

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+
+        const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+        if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+        currentlyActiveAccordionItemHeader.classList.toggle("active");
+        currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        }
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+
+    });
+});
+
+
+
+function check() {
+    var checkBox = document.getElementById("checbox");
+    var text1 = document.getElementsByClassName("text1");
+    var text2 = document.getElementsByClassName("text2");
+
+    for (var i = 0; i < text1.length; i++) {
+        if (checkBox.checked == true) {
+            text1[i].style.display = "block";
+            text2[i].style.display = "none";
+        } else if (checkBox.checked == false) {
+            text1[i].style.display = "none";
+            text2[i].style.display = "block";
+        }
+    }
+}
+check();
